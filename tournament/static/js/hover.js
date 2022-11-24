c79935d0904739ea485a5b333fc63790497d4f2b
hiroshungo <ngoshima.hiro@gmail.com>
2022-11-24 14:06:33 +0900

    変更

--- tournament/static/js/hover.js
+++ tournament/static/js/hover.js
@@ -1,20 +1,22 @@
 const list = document.querySelector(".list");
-//リスト要素の子要素（li）をすべて取得する
+
 const listChildren = list.children;
 let el = document.getElementById("app");
+let el2 = document.getElementById("app2");
+
+
+
+
 
 for (let item of listChildren) {
-    if (el.classList != ''){
+    if (el2.classList != ''){
         item.addEventListener("mouseover",
         
             function () {
                 let target = item.dataset.target;
-            //id="app"の要素を取得
-                
-            //id="app"のclassをクリア
-                el.classList = "";
-            //id="app"に上で取得したtargetをclass属性で追加
-                el.classList.add(target);
+
+                el2.classList = "";
+                el2.classList.add(target);
 
                     
         
@@ -24,18 +26,20 @@ for (let item of listChildren) {
         item.addEventListener('mouseout', 
             function () {
                     
-        //             //id="app"の要素を取得
+                    el2.classList = "";
+                    el2.className = 'explanation2';
                     
-        //             //id="app"のclassをクリア
-                    el.classList = "";
-                    el.className = 'explanation';
-        //             el.classList.add(list1);
-        //             //id="app"に上で取得したtargetをclass属性で追加
                     
         
                  },false);
         } 
     else{
-        el.classList.add(list1);
+        el2.classList.add(list1);
         }
     }
+
+
+
+
+
+
